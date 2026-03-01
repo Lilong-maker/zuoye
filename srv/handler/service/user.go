5b9f44d1129956b7b34b52b25aa64df6948f9d84
pengyilong <3389926937@qq.com>
2026-03-01 15:03:14 +0800

    测试1

--- srv/handler/service/user.go
+++ srv/handler/service/user.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"context"
+	"zuoye/srv/dasic/config"
+	"zuoye/srv/handler/model"
 
 	__ "zuoye/srv/dasic/proto"
 )
@@ -14,5 +16,28 @@ type Server struct {
 // SayHello implements helloworld.GreeterServer
 func (s *Server) OrderAdd(_ context.Context, in *__.OrderAddReq) (*__.OrderAddResp, error) {
 
-	return &__.OrderAddResp{}, nil
+	var order model.Order
+	err := order.FindOrder(config.DB, in.Name)
+	if err != nil {
+		return &__.OrderAddResp{
+			Msg:  "参数错误",
+			Code: 400,
+		}, nil
+	}
+	m := model.Order{
+		Name:  in.Name,
+		Price: float64(in.Price),
+		Num:   int(in.Num),
+	}
+	err = m.OrderAdd(config.DB)
+	if err != nil {
+		return &__.OrderAddResp{
+			Msg:  "订单添加失败",
+			Code: 400,
+		}, nil
+	}
+	return &__.OrderAddResp{
+		Msg:  "订单添加成功",
+		Code: 400,
+	}, nil
 }
